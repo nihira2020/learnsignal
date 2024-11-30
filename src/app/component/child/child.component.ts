92ef6173f24a1c94441877a41f17b6b64881b72e
Natarajan <nataraj.itsolution@gmail.com>
2024-11-30 13:34:26 +0530

    siganl

--- src/app/component/child/child.component.ts
+++ src/app/component/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, computed, effect, EventEmitter, input, Input, OnChanges, output, Output, SimpleChanges } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -8,21 +8,33 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
-export class ChildComponent implements OnChanges {
+export class ChildComponent {
 
-  ngOnChanges(changes: SimpleChanges): void {
-    this.areaValue=this.height * this.width;
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   this.areaValue = this.height * this.width;
+  // }
+
+  // @Input({ required: true }) height: number = 0;
+  // @Input({ required: true }) width: number = 0;
+
+  height = input.required<number>({alias:'ht'});
+  width = input.required<number>({alias:'wt'});
+
+  //@Output() transferdata = new EventEmitter<number>()
+
+  transferdata = output<number>({alias:'getdata'})
+
+  //areaValue = this.height * this.width;
+  areaValue = computed(() => this.height() * this.width())
+
+  constructor(){
+    effect(()=>{
+      this.transferdata.emit(this.areaValue())
+    })
   }
 
-  @Input({ required: true }) height: number = 0;
-  @Input({ required: true }) width: number = 0;
 
-  @Output() transferdata=new EventEmitter<number>()
-
-  areaValue=this.height * this.width;
-
-
-  SendData(){
-   this.transferdata.emit(this.areaValue)
+  SendData() {
+    this.transferdata.emit(this.areaValue())
   }
 }
